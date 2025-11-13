3ae06b41efb4689112be0e5ed979215707e8c2ec
idn-fahmifn <fahmifn.idn@gmail.com>
2025-11-13 17:10:18 +0700

    4.16

--- resources/views/item/detail.blade.php
+++ resources/views/item/detail.blade.php
@@ -44,18 +44,20 @@
                             </p>
                         </div>
 
-                        <div class="pt-4 flex space-x-3">
-                            <button
-                                class="px-4 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700 transition duration-150 shadow-md">
-                                Hapus
-                            </button>
-
-                        </div>
+                        <form action="{{ route('barang.destroy', $data->id) }}" method="post" class="py-6">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" onclick="return confirm('Yakin mau dihapus?')"
+                                class="rounded-md bg-red-500 px-2.5 py-1.5 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20">Hapus</button>
+                            <button x-data="" x-on:click.prevent="$dispatch('open-modal', 'show-edit')"
+                                class="rounded-md bg-yellow-500 px-2.5 py-1.5 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20">Edit</button>
+                        </form>
                     </div>
 
                     <div class="order-1 md:order-2">
-                        <img src="{{ asset('storage/images/items/'.$data->image) }}" alt="Gambar Barang: Server Cisco"
-                            class="w-[300px] h-auto object-cover rounded-lg shadow-xl border-4 border-gray-700 dark:border-gray-600">
+                        <img src="{{ asset('storage/images/items/' . $data->image) }}" alt="Gambar Barang: Server Cisco"
+                            class="h-auto object-cover rounded-lg shadow-xl border-4 border-gray-700 dark:border-gray-600"
+                            width="300">
                     </div>
 
                 </div>
@@ -68,8 +70,63 @@
             @csrf
             @method('put')
 
+            <div class="space-y-4">
+                <div>
+                    <x-input-label value="Nama Barang" />
+                    <x-text-input name="nama_barang" value="{{ $data->item_name }}" type="text"
+                        class="mt-1 block w-full" placeholder="Contoh: Server Cisco" required />
+                </div>
+                <div>
+                    <x-input-label value="Kode Barang" />
+                    <x-text-input name="kode_barang" value="{{ $data->item_code }}" type="number"
+                        class="mt-1 block w-full" placeholder="Contoh: 12345" required />
+                </div>
 
+                <div>
+                    <x-input-label value="Penyimpanan" />
+                    <select name="penyimpanan" required
+                        class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+                        <option value="{{ $data->room_id }}">-{{ $data->room->room_name }}-</option>
+                        @foreach ($room as $item)
+                            <option value="{{ $item->id }}">{{ $item->room_name }}</option>
+                        @endforeach
+                    </select>
+                </div>
 
+                <div>
+                    <x-input-label value="Kondisi Barang" />
+                    <select name="kondisi" required
+                        class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+                        <option value="{{ $data->condion }}">
+                            @switch($data->condition)
+                                @case('good')
+                                    Baik
+                                @break
+                                @case('broke')
+                                    Rusak
+                                @break
+                                @default
+                                Sedang Diperbaiki
+                            @endswitch
+                        </option>
+                        <option value="good">Baik</option>
+                        <option value="broke">Rusak</option>
+                        <option value="maintenance">Perbaikan</option>
+                    </select>
+                </div>
+
+                <div>
+                    <x-input-label value="Harga Barang" />
+                    <x-text-input name="harga" type="number" value="{{ $data->price }}" class="mt-1 block w-full" placeholder="Contoh: 30000"
+                        required />
+                </div>
+
+                <div>
+                    <x-input-label value="Gambar" />
+                    <x-text-input name="gambar" accept="image/*" type="file"
+                        class="mt-1 block w-full border p-4 border-dotted" placeholder="Contoh: 30000" />
+                </div>
+            </div>
 
             <div class="mt-6 flex justify-end">
                 <x-secondary-button x-on:click="$dispatch('close')">
